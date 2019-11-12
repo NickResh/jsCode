8c2a54831b2b7bbae64f5c2c0e20ed98d2bd53d3
NickResh <52072466+NickResh@users.noreply.github.com>
2019-11-12 20:02:53 +0200

    Add files via upload

---
+++ MyTest.js
@@ -0,0 +1,19 @@
+/**
+ * @author nickitar
+ */
+
+//alert ("Hello");
+//console.log ("Hello, world!");
+//document.write("Text");
+
+/*let admin;
+let name = "Джон";
+admin = name;
+alert (admin);*/
+let question = prompt ("Какое официальное название JS?", "");
+if (question == "ECMAScript"){
+    alert("Верно");
+}
+else{
+    alert("Не знаете? ECMAScript!");
+}
