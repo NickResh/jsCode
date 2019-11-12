36e398f42abcfb78eac0caa8c3ca189cc62a9159
NickResh <52072466+NickResh@users.noreply.github.com>
2019-11-12 22:27:27 +0200

    Update MyTest2.js

--- MyTest2.js
+++ MyTest2.js
@@ -1,1 +1,11 @@
-
+function readNumber () {
+    this.number = +prompt ("Введите число", 0);
+    if (typeof (this.number) === typeof number && this.number !=0){
+        alert (this.number);
+        console.log (this.number);
+    }
+    else {
+        readNumber ();
+    }
+}
+readNumber ();
