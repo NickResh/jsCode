e5849268b7127b52dc7e54e67a9850822f44be4b
NickResh <52072466+NickResh@users.noreply.github.com>
2019-11-13 17:24:03 +0200

    Update MyTest2.js

--- MyTest2.js
+++ MyTest2.js
@@ -1,11 +1,49 @@
-function readNumber () {
-    this.number = +prompt ("Введите число", 0);
-    if (typeof (this.number) === typeof number && this.number !=0){
-        alert (this.number);
-        console.log (this.number);
+function checkSpam (str) {
+    
+    let lowCaseStr=str.toLowerCase();
+    if (lowCaseStr.indexOf(`viagra`) !=-1 || lowCaseStr.indexOf(`xxx`) !=-1){
+    console.log (lowCaseStr);
+    alert (`true`);
+    return true;
+
     }
+   
     else {
-        readNumber ();
-    }
+        console.log (lowCaseStr);
+    alert (`False`);
+    return false;
 }
-readNumber ();
+}
+
+
+
+checkSpam('buy ViAgRA now') == true
+checkSpam('free xxxxx') == true
+checkSpam("innocent rabbit") == false
+
+//Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+
+/*function ucFirst (str) {
+    if (!str) return str;
+
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+
+alert (ucFirst (`;№;№!`));*/
+
+
+/*function readNumber () {
+
+    do {
+        this.number = +prompt ("Введите число:", 0);
+    }
+    while (!isFinite(this.number));
+
+    if (this.number === 0 || this.number === null) return null;
+
+    return this.number;
+
+}
+
+alert (`Число- ${readNumber()} $`);*/
