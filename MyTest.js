b4784a443f39c1945844491463e114c03cb26c40
NickResh <52072466+NickResh@users.noreply.github.com>
2019-11-13 17:24:00 +0200

    Update MyTest.js

--- MyTest.js
+++ MyTest.js
@@ -1,19 +1,22 @@
-/**
- * @author nickitar
- */
-
-//alert ("Hello");
-//console.log ("Hello, world!");
-//document.write("Text");
-
-/*let admin;
-let name = "Джон";
-admin = name;
-alert (admin);*/
-let question = prompt ("Какое официальное название JS?", "");
-if (question == "ECMAScript"){
-    alert("Верно");
+function extractCurrencyValue(str){
+    let valueFromString = +str.slice(1);
+    return valueFromString;
 }
-else{
-    alert("Не знаете? ECMAScript!");
+alert (extractCurrencyValue('$12032'));
+
+/*Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, если она превосходит maxlength, 
+заменяет конец str на "…", так, чтобы её длина стала равна maxlength.
+
+Результатом функции должна быть та же строка, если усечение не требуется, либо, если необходимо, усечённая строка.
+function truncate (str, maxlength){
+    let stringLenght = str.length;
+    if (stringLenght>=maxlength){
+        let a = str.substr (0, maxlength);
+        let b = a.slice(0, maxlength-1) + "...";
+        return alert (b);
+    }
+    else return alert(str);
 }
+//truncate();
+truncate("Вот, что мне хотелось бы сказать на эту тему:", 20);
+truncate("Всем привет!", 20);*/
