2b876e0a6ff89d8a44782360e309c12826d59111
NickResh <52072466+NickResh@users.noreply.github.com>
2019-11-21 17:36:19 +0200

    Update htmlAcademy.js

--- htmlAcademy.js
+++ htmlAcademy.js
@@ -1,6 +1,57 @@
+var numbers = [1, 4, 5, 9, 2, 5, 1];
+var uniqueNumbers = [];
+
+for (var i=0; i<numbers.length; i++){
+   if (numbers.indexOf([i],i+1) =-1){
+      uniqueNumbers.push(numbers[i]);
+   }
+console.log(uniqueNumbers);
+}
 
 
-// Алфавит
+/*var indicators = ['сила', 'гибкость', 'выносливость', 'скорость', 'ловкость'];
+var levels = [8, 15, 9, 12, 11];
+var trainingTime = 30;
+var indexElem;
+
+if (trainingTime<=30){
+  indexElem = indicators.indexOf('скорость');
+  levels.splice(indexElem, 1, levels[indexElem]+3);
+  indexElem = indicators.indexOf('ловкость');
+  levels.splice(indexElem, 1, levels[indexElem]+3);
+  }
+  else if (trainingTime>30 && trainingTime<=60){
+    indexElem = indicators.indexOf('гибкость');
+    levels.splice(indexElem, 1, levels[indexElem]+3);
+    }
+    else if(trainingTime>60){
+      indexElem = indicators.indexOf('сила');
+      levels.splice(indexElem, 1, levels[indexElem]+2);
+      indexElem = indicators.indexOf('выносливость');
+      levels.splice(indexElem, 1, levels[indexElem]+2);
+      }
+  console.log (levels);
+*/
+
+/* Техническое задание
+
+Напиши программу, которая будет следить за моими спортивными показателями.
+
+В переменную indicators записан массив с моими спортивными характеристиками.
+
+В массив levels записаны уровни каждого показателя. Каждый элемент этого массива соответствует по индексу элементу массива indicators.
+
+В зависимости от времени тренировки, прокачиваются разные показатели. Время тренировки указано в минутах и записано в переменную trainingTime.
+
+Если тренировка длится до 30 минут включительно, увеличивай скорость и ловкость на 3.
+
+Если тренировка от 30 минут (не включая это значение) до одного часа (включая это значение), увеличивай гибкость на 3.
+
+Если тренировка длится больше одного часа (не включая это значение), увеличивай силу и выносливость на 2.
+
+*/
+
+/*// Алфавит
 var symbols = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ', '.', ',', '—', '!'];
 
 // Смещение
@@ -21,4 +72,4 @@ for (var i=0; i<encodedMessage.length; i++){
   a = encodedMessage[i]+10;
   decodedMessage += symbols[a];
   }
-  console.log (decodedMessage);
+  console.log (decodedMessage);*/
